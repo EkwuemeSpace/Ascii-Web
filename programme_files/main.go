aa3535a79e2d4490b3ac3a9d7d115f517e76c6c0
EkwuemeSpace <innocentekwueme79@gmail.com>
2026-06-16 00:58:22 +0100

    udated go mod

--- programme_files/main.go
+++ programme_files/main.go
@@ -9,12 +9,12 @@ func main() {
 	http.HandleFunc("/", homeHandler)
 	http.HandleFunc("/ascii-art", asciiHandler)
 
-	//Look inside the folder named static in my project.
+	//Look inside the folder named static in my project to load it contents. fs means file server
 	fs := http.FileServer(http.Dir("./static"))
 	//Turn my folder into a mini web server for files
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	log.Println("staring seerver on port :8080")
+	log.Println("staring server on port :8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Println(err)
 		return
